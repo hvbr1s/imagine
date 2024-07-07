9976265607fd034e2b9d35cdb872d80ea9e708b0
Daniel JAHENY <danieljaheny@Daniel-MacBook-Pro.local>
2024-07-07 23:00:35 +0200

    spring  cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import React, { useState } from 'react';
-import './App.css';
-import './index.css';
+import React from 'react';
+import './styles/index.css';
 import ImagineApp from './ImagineApp';
 
 const App: React.FC = () => {
-  const [isProcessing, setIsProcessing] = useState(false);
-
   console.log('App rendering');
-
   return (
     <ImagineApp />
   );
--- src/ImagineApp.tsx
+++ src/ImagineApp.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '@solana/wallet-adapter-react-ui/styles.css';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 import { useWallet } from '@solana/wallet-adapter-react';
